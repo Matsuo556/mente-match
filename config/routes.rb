f1c657e45d1455741b1a6c13a92dee82933d6d32
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-15 08:42:23 +0900

    sign_up view編集中
    ・コントローラー生成
    ・ビュー生成
    ・deviseルーティング記述
    ・users_resistration ストロングパラメーター記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :biz_users
-  devise_for :users
+  devise_for :biz_users, controllers: {
+    sessions:      'biz_users/sessions',
+    #passwords:     'biz_users/passwords',
+    #registrations: 'biz_users/registrations'
+  }
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   get 'pages/index'
   root to: 'pages#index'
 end
