01f597568b87007b25ec99da68ff43410cb6be9b
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-16 11:27:22 +0900

    seed.rbでbiz_usersテーブルにデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-BizUser.create(name: '栃木自動車', email: 'biz@biz', password: 'tochigi', postal_code: '123-4567', prefecture: '栃木県', city: '宇都宮市', : 'biz@biz', address: '川向町1-1-1',building: '宇都宮駅ビル', phone_number: '宇都宮駅ビル')
+BizUser.create(name: '栃木自動車', email: 'biz@biz', password: 'tochigi', postal_code: '123-4567', prefecture: '栃木県', city: '宇都宮市', address: '川向町1-1-1', building: '宇都宮駅ビル', phone_number: '宇都宮駅ビル')
 #  BizUser.find_or_create_by(id: 1) do |biz_user|
 #   biz_user.email = "example@example.com"
 #   biz_user.password = "example"
