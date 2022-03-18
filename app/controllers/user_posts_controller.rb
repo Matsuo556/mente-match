221a7b28fe084f52411ee6565a7f509d082e769a
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-18 18:22:45 +0900

    検索機能( メニュー　コメント)

--- app/controllers/user_posts_controller.rb
+++ app/controllers/user_posts_controller.rb
@@ -2,9 +2,24 @@ class UserPostsController < ApplicationController
 
 def index
   @user_posts = UserPost.includes(:user).order("created_at DESC").page(params[:page]).per(10)
+  @q = UserPost.ransack(params[:q])
+end
+
+def search
+    # params[:q]がnilではない且つ、params[:q][:name]がnilではないとき（商品名の欄が入力されているとき）
+    # if params[:q] && params[:q][:name]と同じような意味合い
+    if params[:q]&.dig(:comment)
+      # squishメソッドで余分なスペースを削除する
+      squished_keywords = params[:q][:comment].squish.delete('　')
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:comment_cont_any] = squished_keywords.split(" ")
+    end
+  @q = UserPost.ransack(params[:q])
+  @user_posts = @q.result.includes(:user).order("created_at DESC").page(params[:page]).per(10)
 end
 
 def new
 end
+
 
 end
