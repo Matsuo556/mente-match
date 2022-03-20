e8cc16d3bf77717af5d25b99b4f299bb928024f9
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-20 18:21:48 +0900

    投稿検索機能の実装

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,4 +2,10 @@ Ransack.configure do |config|
   config.add_predicate 'lteq_end_of_day',
                   arel_predicate: 'lteq',
                   formatter: proc { |v| v.end_of_day }
+  
+  #特定日付で検索したい場合以下predicateを使う
+  # config.add_predicate 'date_eq',
+  #           arel_predicate: 'eq',
+  #           formatter: proc { |v| v.to_date }
+
 end
