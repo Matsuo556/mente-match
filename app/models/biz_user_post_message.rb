25ebe717330e7226e39ab79451f3ba8e63d77c34
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-23 16:03:53 +0900

    バリデーションを記述

--- app/models/biz_user_post_message.rb
+++ app/models/biz_user_post_message.rb
@@ -1,4 +1,8 @@
 class BizUserPostMessage < ApplicationRecord
   belongs_to :post_room
   belongs_to :biz_user
+
+  validates :content,       presence: true
+  validates :post_room_id,  presence: true
+  validates :biz_user_id,   presence: true
 end
--- app/models/user_post_message.rb
+++ app/models/user_post_message.rb
@@ -2,4 +2,8 @@ class UserPostMessage < ApplicationRecord
   belongs_to :post_room
   belongs_to :user
 
+  validates :content,       presence: true
+  validates :post_room_id,  presence: true
+  validates :user_id,       presence: true
+  
 end
