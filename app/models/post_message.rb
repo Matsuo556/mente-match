3d0df837606609ead23cf54c792e718b8504913e
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-25 14:43:44 +0900

    PostMessageモデルにバリデーションを記載

--- app/models/post_message.rb
+++ app/models/post_message.rb
@@ -1,2 +1,8 @@
 class PostMessage < ApplicationRecord
+  belongs_to :post_room
+
+  validates :content,       presence: true
+  validates :is_user,       inclusion: { in: [true, false] }
+  validates :post_room_id,  presence: true
+
 end
