306d63e0197123e3aedb6e8e6bce5c73c0727798
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-28 16:59:44 +0900

    post_room削除機能の実装

--- app/models/post_room.rb
+++ app/models/post_room.rb
@@ -3,8 +3,6 @@ class PostRoom < ApplicationRecord
   belongs_to :user
   belongs_to :biz_user
   has_many :post_messages, dependent: :destroy
-  # has_many :user_post_messages
-  # has_many :biz_user_post_messages
 
   validates :user_post_id,  presence: true
   validates :user_id,       presence: true
