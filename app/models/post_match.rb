2e28b47cf5be8542cfae76bac8fad8ea78e9028b
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-28 17:31:22 +0900

    モデルにバリデーション記載

--- app/models/post_match.rb
+++ app/models/post_match.rb
@@ -1,2 +1,8 @@
 class PostMatch < ApplicationRecord
+  belongs_to :post_room
+
+  validates :maintain_at,  presence: true
+  validates :charge,       presence: true
+  validates :post_room_id,   presence: true
+
 end
