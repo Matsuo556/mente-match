b4e0c7894f54bb52afefe5f428cfc3fc371b8684
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-30 19:29:50 +0900

    post_messagesコントローラーのcreateアクション記述変更
    post_matchがあるときはpost_match showアクションへ
    
    post_matchがないときはpost_room indexアクションへ

--- app/controllers/post_messages_controller.rb
+++ app/controllers/post_messages_controller.rb
@@ -10,11 +10,21 @@ class PostMessagesController < ApplicationController
       @post_message.is_user = false
     end
 
-    if @post_message.save
-      redirect_to user_post_post_rooms_path(params[:user_post_id])
-    else
-      redirect_to user_post_post_rooms_path(params[:user_post_id])
+    post_room = @post_message.post_room
+    #マッチングが成立している場合はpost_matchのshowアクションへ
+    if @post_message.save && post_room.post_match
+      redirect_to user_post_post_room_post_match_path(params[:user_post_id], post_room.id, post_room.post_match.id) and return
+    elsif post_room.post_match
+      redirect_to user_post_post_room_post_match_path(params[:user_post_id], post_room.id, post_room.post_match.id) and return
     end
+
+    #マッチングが成立していない場合はpost_roomのindexアクションへ
+    if @post_message.save
+      redirect_to user_post_post_rooms_path(params[:user_post_id]) and return
+    else
+      redirect_to user_post_post_rooms_path(params[:user_post_id]) and return
+    end
+
   end
 
 
