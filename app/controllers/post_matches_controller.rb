cc7c55da8dd533c82d8bb083d65e018ee8479f40
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-30 11:23:05 +0900

    PostMatchテーブルに保存できることを確認

--- app/controllers/post_matches_controller.rb
+++ app/controllers/post_matches_controller.rb
@@ -12,9 +12,17 @@ class PostMatchesController < ApplicationController
   end
 
   def create
+    @post_match = PostMatch.new(post_match_params)
+    unless @post_match.save
+      render :new
+    end
   end
 
 
+  private
 
+  def post_match_params
+    params.require(:post_match).permit(:maintain_at, :menu_id, :comment, :charge).merge(post_room_id: params[:post_room_id])
+  end
 
 end
