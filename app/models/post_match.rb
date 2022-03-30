e918a641e4cbc95240842568bfdf51f3254fa29e
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-30 10:54:08 +0900

    PostMatchモデルにActiveHashの記載
    menu_idのバリデーションも追加

--- app/models/post_match.rb
+++ app/models/post_match.rb
@@ -1,9 +1,12 @@
 class PostMatch < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :post_room
 
   validates :maintain_at,  presence: true
   validates :charge,       presence: true, numericality: { only_integer: true, message: 'is invalid. Input half-width number.' }
   validates :post_room_id,   presence: true
+  validates :menu_id, numericality: { other_than: 1, message: "can't be blank"}
+
 
 
 end
