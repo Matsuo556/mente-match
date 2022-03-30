d9b62add12bb464a91ec41fa85f32f03e8b9ec4f
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-30 09:39:11 +0900

    post_matchs newアクションのビュー表示完了

--- app/controllers/post_matches_controller.rb
+++ app/controllers/post_matches_controller.rb
@@ -3,12 +3,18 @@ class PostMatchesController < ApplicationController
 
   def index
   end
-  
+
 
   def new
+    @user_post = UserPost.find(params[:user_post_id])
+    @post_room = PostRoom.find(params[:post_room_id])
+    @post_match = PostMatch.new
   end
 
   def create
   end
+
+
+
 
 end
--- app/models/post_match.rb
+++ app/models/post_match.rb
@@ -2,7 +2,8 @@ class PostMatch < ApplicationRecord
   belongs_to :post_room
 
   validates :maintain_at,  presence: true
-  validates :charge,       presence: true
+  validates :charge,       presence: true, numericality: { only_integer: true, message: 'is invalid. Input half-width number.' }
   validates :post_room_id,   presence: true
+
 
 end
