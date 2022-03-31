514dd627149bc22a708494f185b15afa3cf5d720
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-03-31 09:55:44 +0900

    チャットのレイアウト修正

--- app/controllers/post_messages_controller.rb
+++ app/controllers/post_messages_controller.rb
@@ -17,7 +17,6 @@ class PostMessagesController < ApplicationController
     elsif post_room.post_match
       redirect_to user_post_post_room_post_match_path(params[:user_post_id], post_room.id, post_room.post_match.id) and return
     end
-
     #マッチングが成立していない場合はpost_roomのindexアクションへ
     if @post_message.save
       redirect_to user_post_post_rooms_path(params[:user_post_id]) and return
