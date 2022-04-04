7b127a568af082bbe71e3b7afb4e51cffeded54d
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-04-04 16:42:13 +0900

    検索機能の実装
    市区町村、店名での検索

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,28 @@ class BooksController < ApplicationController
   
   def index
     @biz_users = BizUser.all.page(params[:page]).per(10)
+    @q = BizUser.ransack(params[:q])
   end
+
+  def search
+    # params[:q]がnilではない且つ、params[:q][:name]がnilではないとき（商品名の欄が入力されているとき）
+    # if params[:q] && params[:q][:name]と同じような意味合い
+    if params[:q]&.dig(:city)
+      # squishメソッドで余分なスペースを削除する
+      city_squished_keywords = params[:q][:city].squish.delete('　')
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:city_cont_any] = city_squished_keywords.split(" ")
+    end
+    if params[:q]&.dig(:name)
+      # squishメソッドで余分なスペースを削除する
+      name_squished_keywords = params[:q][:name].squish.delete('　')
+      ## 半角スペースを区切り文字として配列を生成し、paramsに入れる
+      params[:q][:name_cont_any] = name_squished_keywords.split(" ")
+    end
+    @q = BizUser.ransack(params[:q])
+    @biz_users = @q.result.page(params[:page]).per(10)
+    binding.pry
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,10 @@ Rails.application.routes.draw do
   resources :users, only: :show
   resources :biz_users, only: :show
 
-  resources :books, only: [:index]
+  resources :books, only: [:index] do
+    collection do
+      get 'search'
+    end
+  end
 
 end
