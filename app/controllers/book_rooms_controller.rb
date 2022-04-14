bb7de4fe6e364ceb7d922ff0d21799693aba7580
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-04-14 17:53:31 +0900

    book_roomsコンローラーへの記述

--- app/controllers/book_rooms_controller.rb
+++ app/controllers/book_rooms_controller.rb
@@ -1,19 +1,39 @@
 class BookRoomsController < ApplicationController
+  before_action :authenticate_any!, only: :index
+  before_action :user_check, only: :index
 
   def index
-    if Book.exists?(params[:book_id])
+    # if Book.exists?(params[:book_id])
       @book = Book.find(params[:book_id])
       @book_room = BookRoom.find(@book.book_room.id)
       @book_messages = @book_room.book_messages
-    else
-      if user_signed_in?
-        redirect_to user_path(current_user.id)
-      else
-        redirect_to biz_user_path(current_biz_user.id)
-      end
-    end
+    # else
+    #   if user_signed_in?
+    #     redirect_to user_path(current_user.id)
+    #   else
+    #     redirect_to biz_user_path(current_biz_user.id)
+    #   end
+    # end
   end
 
 
+  private
+
+  def authenticate_any!
+    if biz_user_signed_in?
+      true
+    else
+      authenticate_user!
+    end
+  end
+
+  def user_check
+    @book = Book.find(params[:book_id])
+    if user_signed_in? && current_user.id != @book.book_room&.user_id
+      redirect_to root_path
+    elsif biz_user_signed_in? && current_biz_user.id != @book.book_room&.biz_user_id
+      redirect_to root_path
+    end
+  end
 
 end
