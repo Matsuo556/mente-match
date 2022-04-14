4a86c8aeaef3508654b31c596412b47b27fb5edf
Matsuo556 <98728897+Matsuo556@users.noreply.github.com>
2022-04-14 09:21:45 +0900

    users showアクションへの記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+  before_action :move_to_root_path, only: [:show]
+  
 
   def show
     @user_posts = UserPost.includes(post_room: :post_match).order("created_at DESC")
@@ -6,4 +9,13 @@ class UsersController < ApplicationController
     @books = Book.includes(:biz_user, book_room: :book_match).order("created_at DESC")
   end
 
+  private
+
+  def move_to_root_path
+    if current_user.id.to_s !=  params[:id]
+      redirect_to root_path
+    end
+  end
+
 end
+
